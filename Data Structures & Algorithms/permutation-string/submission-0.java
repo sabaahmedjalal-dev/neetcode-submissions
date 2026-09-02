d0696bb465324f6c69844632ec724fe907991841
saba_aj <sabaahmedjalal@gmail.com>
2026-09-02 16:14:07 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        s1 = sort(s1);
+        for(int i=0 ; i <= s2.length()-s1.length() ; i++){
+            if(s1.equals(sort(s2.substring(i, i+s1.length()))))
+                return true;
+        }
+        return false;
+    }
+    String sort(String s){
+        char[] charStr = s.toCharArray();
+        Arrays.sort(charStr);
+        return new String(charStr);
+    }
+}
